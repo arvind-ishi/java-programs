8307d6e6cf7b859561d3cf6d85c9ab3aead97269
arvind-ishi <avij70507@gmail.com>
2024-02-17 17:51:17 +0530

    this is the frist time the java code is posting and learn the java

---
+++ JavaProv.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+
+public class JavaProv{
+ public static void main(String [] args){
+  System.out.print("the kg coding in java");
+
+ }
+}
---
+++ app/sum.java
@@ -0,0 +1,14 @@
+
+
+package app;
+
+public class sum{
+    public static void main(String[] args) {
+
+        int a = 22, b = 44, sum = 0;
+
+        sum = a + b;
+        System.out.println("sum is the a and b is :" + sum);
+
+    }
+}
